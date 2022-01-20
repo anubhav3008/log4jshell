e9bfd7e87525a684ce94f61f94476a4f28be9a66
Anubhav <anubhav.workemail@gmail.com>
2022-01-20 06:54:12 +0530

    first commit

---
+++ src/main/java/com/anubhav/Log4jVulnApplication.java
@@ -0,0 +1,15 @@
+package com.anubhav;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4jVulnApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Log4jVulnApplication.class, args);
+
+
+	}
+
+}
